ff7f222e6d39502add0a4c71ad818fef458094b0
赖信涛 <laixintao1995@163.com>
2017-07-02 16:10:10 +0800

    release version v0.2.1

--- README.md
+++ README.md
@@ -6,18 +6,22 @@
 
 A git extension that allows you submit pullrequests from command line.
 
+## install
+
+    pip install git-ext
+
 ## usage
 
-    $ git pullrequests create test_b master
+    $ git pullrequest create test_b master
     Reviewers(start with @):
     201 Created!
     #2 This is your commit title.[test_b->master]  by boson_laixintao(just now)
     Reviewers:
 
-    $ git pullrequests list
+    $ git pullrequest list
     #2 This is your commit title.[test_b->master]  by boson_laixintao(just now)
 
-    $ git pullrequests
+    $ git pullrequest
     Usage: git-pullrequests [OPTIONS] COMMAND [ARGS]...
 
     Options:
@@ -27,4 +31,3 @@ A git extension that allows you submit pullrequests from command line.
       activity  Show a pr's activity, display lastest 10...
       create
       list
-     ~/Documents/program/program/test-gitext   test_b  git pullrequests --help
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from codecs import open
 from setuptools import setup, find_packages
 
-VERSION = 0.1
+VERSION = '0.2.2'
 DESCRIPTION = 'A git extension that allows you submit pullrequests from command line.'
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -29,22 +29,6 @@ setup(
     install_requires=requires,
     license='GNU',
     zip_safe=False,
-    classifiers=(
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-        ),
     # TODO use install to generic yml file cmdclass={'git-pullrequests': PyTest},
     entry_points={
         'console_scripts': [
