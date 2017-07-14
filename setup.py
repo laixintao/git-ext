bba27bb959a649883521783934597f1f9b556d2e
laixintao <glenn.lai@bosondata.com.cn>
2017-07-14 16:32:49 +0800

    release new version v$NEW_VERSION

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from codecs import open
 from setuptools import setup, find_packages
 
-VERSION = '0.2.11'
+VERSION = '0.2.12'
 DESCRIPTION = 'A git extension that allows you submit pullrequests from command line.'
 
 here = os.path.abspath(os.path.dirname(__file__))
