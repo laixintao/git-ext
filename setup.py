7fd1ff2237553c5347022bc23c4618c5731930a2
laixintao <glenn.lai@bosondata.com.cn>
2017-11-06 19:34:42 +0800

    Bump version: 0.3.11 → 0.3.12

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from codecs import open
 from setuptools import setup, find_packages
 
-VERSION = '0.3.11'
+VERSION = '0.3.12'
 DESCRIPTION = 'A git extension that allows you submit pullrequests from command line.'
 
 here = os.path.abspath(os.path.dirname(__file__))
